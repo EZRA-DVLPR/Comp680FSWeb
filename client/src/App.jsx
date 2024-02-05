a85c3accce3840f02701de8c1f8f24340e9787e2
EZRA-DVLPR <derpisontr@gmail.com>
2024-02-05 13:07:45 -0800

    added pages to fronted for utilizing backend features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import CreateFile from './pages/CreateFiles';
+import DeleteFile from './pages/DeleteFile';
+import EditFile from './pages/EditFile';
+import DeleteFile from './pages/DeleteFile';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/files/create' element={<CreateFile />} />
+      <Route path='/files/details/:id' element={<ShowFile />} />
+      <Route path='/files/edit/:id' element={<EditFile />} />
+      <Route path='/files/delete/:id' element={<DeleteFile />} />
+    </Routes>
+    
+  );
+};
 
-export default App
+export default App;
+
+//{<div className='bg-red-400 text-white'>App</div>}
---
+++ client/src/pages/ShowFile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowFile = () => {
+  return (
+    <div>ShowFile</div>
+  )
+}
+
+export default ShowFile
