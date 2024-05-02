d021447cffb3730c1364097c6ded0d7e1731da77
Arian <arianxdehghani@gmail.com>
2024-05-01 18:49:27 -0700

    ???

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default ({mode}) => {
   const env = loadEnv(mode, process.cwd(), '');
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  return {
+  return defineConfig({
     plugins: [react()],
     server:  {
       host: '0.0.0.0', // or true
@@ -13,5 +13,5 @@ export default defineConfig(({ command, mode }) => {
     define: {
         HOST_URI: process.env.HOST_URI,
     },
-  }
-})
+  })
+}
