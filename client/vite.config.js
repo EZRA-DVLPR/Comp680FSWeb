af631d95022d9556bff09e2c34220683f75b70f0
Arian <arianxdehghani@gmail.com>
2024-05-01 18:38:40 -0700

    Add missing comma

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server:  {
     host: '0.0.0.0', // or true
-  }
+  },
   define: {
       'process.env.HOST_URI': JSON.stringify(env.HOST_URI),
   },
