013bc32e5b47ef7da1abdef65299d9d178616e56
Arian <arianxdehghani@gmail.com>
2024-05-01 18:46:33 -0700

    Directly read env var

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return {
     plugins: [react()],
     server:  {
       host: '0.0.0.0', // or true
     },
     define: {
-        HOST_URI: JSON.stringify(env.HOST_URI),
+        HOST_URI: process.env.HOST_URI,
     },
   }
 })
